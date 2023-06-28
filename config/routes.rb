b0a5256dcac9082892945865c727880235738b82
magali mares <maga.mares@gmail.com>
2023-06-28 15:48:09 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
